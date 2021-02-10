e8515fe10e53d29a65550b19678a281ce74edd27
Tami Ribeiro <tamiribeiros@hotmail.com>
2021-02-09 22:33:45 -0300

    able to make outcomes only with valid income balance

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -23,6 +23,12 @@ class CreateTransactionService {
     const transactionRepository = getCustomRepository(TransactionsRepository);
     const categoryRepository = getRepository(Category);
 
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
     let transactionCategory = await categoryRepository.findOne({
       where: {
         title: category,
